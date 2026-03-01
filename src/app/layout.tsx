b54454019edcaab78807af0a2547d7fb35c46565
Brannon McAllister <brannon@Brannons-Mac-Studio.local>
2026-03-01 16:52:00 -0500

    Add socialshare.jpg as OG and Twitter card image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,10 +14,12 @@ export const metadata: Metadata = {
   openGraph: {
     type: 'website',
     siteName: 'The Gospel Coalition',
+    images: [{ url: '/socialshare.jpg', width: 1200, height: 630 }],
   },
   twitter: {
     card: 'summary_large_image',
     site: '@TGC',
+    images: ['/socialshare.jpg'],
   },
 }
 
